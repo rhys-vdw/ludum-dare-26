34a431eeaa60d7aa7397bcdc16256751811469b4
Tristan Gamilis <tristan@2suggestions.com.au>
2013-04-27 15:35:56 +1000

    Added sass support and base stylesheet

--- web.rb
+++ web.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require 'haml'
+require 'compass'
+require 'sass'
 
-set :haml, :format => :html5
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
+get '/stylesheets/style.css' do
+  sass :style
+end
+
 
 get '/' do
 	haml :index
